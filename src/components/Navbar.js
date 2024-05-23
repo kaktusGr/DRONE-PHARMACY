cdb2e30196b77b72c059382905e903822b4bdac9
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-05-23 16:04:55 +0200

    Create Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <header>Navbar</header>
+        <header>
+            <div className="header-help-btn">
+                <ul>
+                    <li>Find a Store</li>
+                    <li>Help</li>
+                    <li>Sign In</li>
+                </ul>
+            </div>
+            <div className="header-main">
+                <Link to="/"><h3>air Pharm</h3></Link>
+                <nav>
+                    <ul>
+                        <li><Link to='catalog'>Catalog</Link></li>
+                        <li><Link to='blog'>Blog</Link></li>
+                        <li><Link to='about'>About</Link></li>
+                        <li><Link to='contacts'>Contacts</Link></li>
+                    </ul>
+                </nav>
+                <div className="header-search">
+                    <input type="text"></input>
+                    <button>
+                        <img src="./images/icons-svg/search.svg" alt="search" />
+                    </button>
+                </div>
+                <div className="header-icons">
+                    <Link to='account-orders'><img src="./images/icons-svg/user.svg" alt="account" /></Link>
+                    <Link to='favourites'><img src="./images/icons-svg/heart.svg" alt="favourites" /></Link>
+                    <Link to='shopping-cart'><img src="./images/icons-svg/cart.svg" alt="cart" /></Link>
+                </div>
+            </div>
+        </header>
     )
 }
