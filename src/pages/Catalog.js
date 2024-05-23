43c320f246f60459eb10772f71659da18995146c
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-05-23 18:49:59 +0200

    Create Discount and DiscountScroll components and add them to Catalog page

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import DiscountScroll from '../components/DiscountScroll';
 
 export default function Catalog() {
     return (
-        <div className='container'>
-            Catalog
-        </div>
+        <>
+            <DiscountScroll
+                props={[
+                    {
+                        id: 1,
+                        title: 'Cashback Offer: 10% Back on Your First Order',
+                        pills: ['best deals', 'Cashback']
+                    },
+                    {
+                        id: 2,
+                        title: 'Free Shipping on Orders Over $50',
+                        pills: ['best deals', 'Free Shipping']
+                    },
+                    {
+                        id: 3,
+                        title: 'Cashback Offer: 10% Back on Your First Order',
+                        pills: ['best deals', 'Cashback']
+                    },
+                    {
+                        id: 4,
+                        title: 'Free Shipping and Cashback on Orders Over $80',
+                        pills: ['best deals', 'Free Shipping', 'Cashback']
+                    }
+                ]} />
+        </>
     )
 }
