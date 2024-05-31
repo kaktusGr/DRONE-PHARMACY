d62be416e7638e41c7c1b4cbf590f1d05c79797f
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-05-31 15:40:38 +0200

    Add styles to Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-export default function Product({ brand, name, gram, price, stars, status }) {
+export default function Product({ name, gram, status }) {
     return (
-        <div className="product-card">
+        <div className={status !== "AVAILABLE" ? "product-card unavailable" : "product-card"}>
+            {status !== "AVAILABLE" && <img src="./images/icons-svg/plus.svg" alt="close" />}
             <div className="product-info">
-                <p className="brand">{brand}</p>
+                <p className="brand">Brand name</p>
                 <div className="product-name">
-                    <p className="name">{name}, {gram}</p>
-                    <p className="price">{price}</p>
+                    <p className="name">{name}, {gram}G</p>
+                    <p className="price">$29.99</p>
                 </div>
                 <div className="product-hover-block">
-                    <img src="./images/icons-svg/star.svg" alt="star" /><b>{stars}</b>
+                    <img src="./images/icons-svg/star.svg" alt="star" /><b>4.3</b>
                     <div className="product-small-info">
                         <small>{status}</small>
                         <div>
@@ -18,7 +19,9 @@ export default function Product({ brand, name, gram, price, stars, status }) {
                             <small>Compare</small>
                         </div>
                     </div>
-                    <button className="dark-btn">Add to Cart</button>
+                    <button className={status !== "AVAILABLE" ? "dark-btn unavailable" : "dark-btn"}>
+                        Add to Cart
+                    </button>
                 </div>
             </div>
         </div>
