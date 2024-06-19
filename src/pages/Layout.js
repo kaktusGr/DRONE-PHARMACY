b5873acd79e307a094cf909e2281eae14f276194
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-06-19 16:00:42 +0200

    Update Layout structure with new components

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Layout() {
     return (
-        <div className='container'>
-            <Outlet />
-        </div>
+        <>
+            <Header />
+            <div className='container'>
+                <Outlet />
+                <Footer />
+            </div>
+        </>
     )
 }
