53cc093c410e4746d2ba60853267e527700f21c4
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-06-19 15:20:11 +0200

    Update App.js with react-router-dom for multi-page project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Orders from './pages/Orders';
+import ShoppingCart from './pages/ShoppingCart';
+import NoPage from './pages/NoPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="catalog" element={<Catalog />} />
+                    <Route path="orders" element={<Orders />} />
+                    <Route path="shopping-cart" element={<ShoppingCart />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </HashRouter>
+    )
 }
-
-export default App;
