0a32589a850015973c5c3f6b6f971e8447a7b7f5
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-06-21 00:19:34 +0200

    Update Layout.js wrapping if in ContextProvider

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Outlet } from "react-router-dom";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { ContextProvider } from '../Context';
 
 export default function Layout() {
     return (
-        <>
+        <ContextProvider>
             <Header />
             <div className='container'>
                 <Outlet />
                 <Footer />
             </div>
-        </>
+        </ContextProvider>
     )
 }
