0f469f0ab4ca52400797e9bde6a8e4c704426db0
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-06-27 14:16:41 +0200

    Return simple setting in component Products to show all products

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,8 +5,6 @@ import { Context } from "../Context";
 export default function Products() {
     const context = useContext(Context);
 
-    console.log(context.allMedicines);
-
     return (
         <div className="catalog-products">
             {context.allMedicines.length ? (
@@ -16,25 +14,4 @@ export default function Products() {
             )}
         </div>
     )
-    // return (
-    //     <div className="catalog-products">
-    //         {context.allMedicines.length ? (
-    //             context.cartItems.length ? (
-    //                 context.allMedicines.map(item => {
-    //                     for (let value of context.cartItems) {
-    //                         if (item.id === value.id) {
-    //                             return <Product key={item.id} status={value.status} {...item} />
-    //                         } else {
-    //                             return <Product key={item.id} {...item} />
-    //                         }
-    //                     }
-    //                 })
-    //             ) : (
-    //                 context.allMedicines.map(item => <Product key={item.id} {...item} />)
-    //             )
-    //         ) : (
-    //             <p>Failed to load product list</p>
-    //         )}
-    //     </div>
-    // )
 }
