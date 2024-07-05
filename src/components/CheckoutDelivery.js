6f16c60e41ae9319066b636301b0a822012b6aa4
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-07-05 20:19:40 +0200

    Add new component CheckoutDelivery with basic settings

---
+++ src/components/CheckoutDelivery.js
@@ -0,0 +1,121 @@
+import { React } from 'react';
+
+export default function CheckoutDelivery() {
+    const now = new Date();
+
+    const getDay = (value) => {
+        const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+        const dateCopy = new Date(now);
+        switch (value) {
+            case "today":
+                break;
+            case "tomorrow":
+                dateCopy.setDate(dateCopy.getDate() + 1);
+                break;
+            case "dayAfterTomorrow":
+                dateCopy.setDate(dateCopy.getDate() + 2);
+                break;
+            default:
+                console.log("Error switch");
+        }
+        return week[dateCopy.getDay()];
+    }
+
+    const setDateAfterTomorrow = () => {
+        const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ];
+        const dateCopy = new Date(now);
+        dateCopy.setDate(dateCopy.getDate() + 2);
+        return `${months[dateCopy.getMonth()]} ${dateCopy.getDate()}`;
+    }
+
+    function handleClick(e) {
+        if (!e.target.closest('.date.active')) {
+            document.querySelectorAll('.active').forEach(item => item.classList.remove('active'));
+        }
+        e.target.closest('.date').classList.toggle('active');
+    }
+
+    return (
+        <div className='checkout-delivery'>
+            <h3>Delivery</h3>
+            <div className='delivery-inputs'>
+                <div id='when'>
+                    <div>
+                        <h4>When to deliver your order?</h4>
+                        <p>Choose a convenient delivery date and time</p>
+                    </div>
+                    <div className='delivery-dates'>
+                        <div className='date active' onClick={handleClick}>
+                            <p>{getDay("today")}</p>
+                            <p>Today</p>
+                        </div>
+                        <div className='date' onClick={handleClick}>
+                            <p>{getDay("tomorrow")}</p>
+                            <p>Tomorrow</p>
+                        </div>
+                        <div className='date' onClick={handleClick}>
+                            <p>{getDay("dayAfterTomorrow")}</p>
+                            <p>{setDateAfterTomorrow()}</p>
+                        </div>
+                    </div>
+                    <ul className='delivery-times'>
+                        <li>
+                            <input type="radio" name="time" value="asap" defaultChecked />
+                            <label htmlFor='asap'>ASAP</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="time" value="8-10" />
+                            <label htmlFor='8-10'>8:00 AM — 10:00 AM</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="time" value="10-12" />
+                            <label htmlFor='10-12'>10:00 AM — 12:00 PM</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="time" value="12-2" />
+                            <label htmlFor='12-2'>12:00 PM — 2:00 PM</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="time" value="2-4" />
+                            <label htmlFor='2-4'>2:00 PM — 4:00 PM</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="time" value="4-6" />
+                            <label htmlFor='4-6'>4:00 PM — 6:00 PM</label>
+                        </li>
+                    </ul>
+                </div>
+                <div id='where'>
+                    <div>
+                        <h4>Where to deliver your order?</h4>
+                        <p>Enter the address or use the map</p>
+                    </div>
+                    <form name="form-address" action="" method="get">
+                        <input type='text' placeholder='Address*'></input>
+                        <input type='text' placeholder='Delivery comment'></input>
+                    </form>
+                    <div className='map'>
+                        <iframe title="map" src="https://yandex.ru/map-widget/v1/?ll=83.051717%2C54.836438&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgoxNTk4MTkxOTM4EkbQoNC-0YHRgdC40Y8sINCd0L7QstC-0YHQuNCx0LjRgNGB0LosINCR0LXRgNC00YHQutC40Lkg0YLRg9C_0LjQuiwgOS8xIgoNcBmmQhUgWFtC&z=17.17" frameBorder="1" allowFullScreen={true} scroll="false"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div className='attention'>
+                <img src="./images/icons/info-circle.svg" alt="attention" />
+                <p>Ensure the delivery area is clear of obstacles. The drone requires a flat, open space to land safely. Recommended dimensions for the landing area are 2×2 meters.</p>
+            </div>
+        </div>
+    )
+}
