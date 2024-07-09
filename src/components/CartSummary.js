aa87a2f60e4713c5fc03a313b6f6ad4be86a8dae
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-07-09 17:49:09 +0200

    Update component CartSummary by adding fetch request for available drones and add function to check drones weight

--- src/components/CartSummary.js
+++ src/components/CartSummary.js
@@ -1,9 +1,29 @@
-import { React, useContext } from 'react';
+import { React, useContext, useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { Context } from "../Context";
 
-export default function CartSummary({ btnType }) {
+export default function CartSummary({ btnType, handleSelectItem }) {
     const context = useContext(Context);
+
+    const [drones, setDrones] = useState([]);
+    const [isBestCapacity, setIsBestCapacity] = useState(false);
+
+    useEffect(() => {
+        let ignore = false;
+        fetch('http://localhost:8090/drone/available', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(result => result.json())
+            .then(data => {
+                if (!ignore) {
+                    setDrones(data);
+                }
+            });
+        return () => {
+            ignore = true;
+        }
+    }, []);
 
     const totalSelected = context.selectedItems ? (
         context.selectedItems.length
@@ -20,6 +40,17 @@ export default function CartSummary({ btnType }) {
             .map(med => med.price)
             .reduce((accum, current) => accum + current, 0)
     ) : 0;
+
+    const checkDronesWeight = () => {
+        const sortDroneWeight = drones
+            .sort((a, b) => a.weightLimit - b.weightLimit)
+        for (let drone of sortDroneWeight) {
+            if (drone.weightLimit >= totalWeight) {
+                return drone.id;
+            }
+        }
+        return null;
+    }
 
     return (
         <div className='cart-summary'>
@@ -44,14 +75,29 @@ export default function CartSummary({ btnType }) {
                     </tr>
                 </tbody>
             </table>
-            {totalSelected === 0 && <p className='summary-error'>
+            {totalSelected === 0 && <p className='summary-error null'>
                 * Please select something to proceed to checkout
             </p>}
+            {checkDronesWeight() === null &&
+                <div className='summary-error overweight'>
+                    <p>* Unfortunately, we don't have a free drone available for the total weight of the selected items. In this case, we may offer you to arrange several deliveries.</p>
+                    <p>Please choose the products yourself or click the button below and we will select the products according to the best capacity.</p>
+                    <div>
+                        <input type='checkbox' name='checkbox' id='checkbox'
+                            checked={isBestCapacity}
+                            onChange={(e) => {
+                                setIsBestCapacity(e.target.checked);
+                            }} />
+                        <label htmlFor='checkbox'>Choose the best capacity</label>
+                    </div>
+                </div>}
             {btnType === "shopping-cart" ? (
                 <>
                     <div className='summary-btns'>
-                        <Link to='/checkout' id='checkout' className={totalSelected === 0 ? "disabled" : undefined}
-                            onClick={(e) => totalSelected === 0 && e.preventDefault()}>
+                        <Link to='/checkout' id='checkout'
+                            className={(totalSelected === 0 || checkDronesWeight() === null) ? "disabled" : undefined}
+                            onClick={(e) => (totalSelected === 0 || checkDronesWeight() === null)
+                                && e.preventDefault()}>
                             Proceed to checkout
                         </Link>
                         <Link to='/catalog' id='catalog'>
