47a26504d36a288d7c8dd371dc9d3a4325897e24
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-07-22 15:16:36 +0200

    Add POST fetch request to create order

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import { React, useContext, useEffect } from 'react';
+import { Context } from "../Context";
 
 export default function Orders() {
+    const context = useContext(Context);
+
+    const order = {
+        "droneId": context.droneId,
+        "medicationItems": context.selectedItems.map(item => item.id),
+    };
+
+    useEffect(() => {
+        let ignore = false;
+        if (context.droneId) {
+            fetch(`http://localhost:8090/delivery/create`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(order)
+            })
+                .then(result => result.json())
+                .then(data => {
+                    if (!ignore) {
+                        console.log(data);
+                        context.remove(order.medicationItems);
+                    }
+                });
+        }
+        return () => {
+            ignore = true;
+        }
+    }, []);
+
     return (
         <div className='orders'>
             <h1>Orders</h1>
