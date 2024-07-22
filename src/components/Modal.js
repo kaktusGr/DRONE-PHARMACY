64e65de1cfa8944d63cf54ba7133d2b2e216efa8
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-07-22 18:13:38 +0200

    Add component Modal

---
+++ src/components/Modal.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+
+export default function Modal({ isOpen, onClose, children }) {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <div className='modal-flex'>
+                    {children}
+                </div>
+                <button onClick={onClose}>
+                    <img id="close" src="./images/icons/plus.svg" alt="close" />
+                </button>
+            </div>
+        </div>
+    )
+}
